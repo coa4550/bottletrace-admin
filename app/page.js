f920abe26b5ada17f2c0892507adcd798eff86b5
Tyler Berry <tyler@coadata.io>
2025-10-05 18:22:51 +0200

    Added Distributors page and sidebar link

--- app/page.js
+++ app/page.js
@@ -3,8 +3,9 @@ export default function Page() {
     <div>
       <h1 style={{ marginTop: 0 }}>Welcome to BottleTrace Admin</h1>
       <ul>
-        <li>Manage <b>Brands</b> and <b>Suppliers</b> easily.</li>
+        <li>Manage <b>Brands</b>, <b>Distributors</b> & <b>Suppliers</b> easily.</li>
         <li>Control <b>Brand ↔ Supplier ↔ State</b> mappings.</li>
+        <li>Control <b>Brand ↔ Distributor ↔ State</b> mappings.</li>
         <li>Bulk upload verified relationships via CSV.</li>
       </ul>
     </div>
